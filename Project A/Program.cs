56c99c775f51bde15de176b0ea391f05440d8708
Shohruh <shohruh.bozorov@edu.newton.se>
2021-10-07 22:06:51 +0200

    made nr of articles function

--- Project A/Program.cs
+++ Project A/Program.cs
@@ -5,83 +5,119 @@ namespace Project_A
     class Program
     {
 
-        
-
         static void Main(string[] args)
         {
+            methods.ChangeConsole();
+            string text1 = "How many articles do you want? (From 1 to 10)";
+            bool result = false;
+            string text2 = "\nWrong input, try again! Only numbers.";
+            
+            int i = 0;
 
-
-            string[] mixedArticles = new string[2];
-            int[] price = new int[2];
-            mixedArticles = Console.ReadLine().Split(';');
-
-            for (int i = 0; i < 6; i++)
+            do
             {
-                if (i % 2 != 0)
-                    Int32.TryParse(mixedArticles[i], out price[i]);
-
-                Console.WriteLine(price[i]);
-
-            }
-
-            //0,1,2,3,4,5,6
-
-
-
-
-            Console.WriteLine(mixedArticles[0]);
-            Console.WriteLine(mixedArticles[1]);
-            Console.WriteLine(price);
+                Console.WriteLine(text1);
+                result = methods.NrOfArticles();
+                if (!result) 
+                {
+                    methods.ChangeFontColor(text2);
+                    Console.WriteLine(text2);
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                
+                }
+                    
 
 
 
-
-            Console.Title = "Project A";
-            Console.WriteLine("Let's print a receipt");
+            } while (result == false);
 
 
-            Articles.InputArticles();
+
+            Console.WriteLine(result);
 
 
-            for (int i = 0; i < Articles.numberOfArticles; i++)
-            {
-
-                Console.WriteLine($"Write the name and the price for article #{i}");
-                Articles.mixedArticles = Console.ReadLine().Split(';');
-
-                Console.WriteLine(Articles.mixedArticles[0]) ;
-               
 
 
-            }
 
 
         }
+
+
 
         class Articles
         {
             public static int numberOfArticles;
-            public static double[] articlePrices = new double[numberOfArticles];
-            public static string[] articelNames = new string[numberOfArticles];
-            public static string[] mixedArticles = new string[numberOfArticles];
 
-            public static void InputArticles()
+            #region arrays
+
+            public static string[] mixedArticles = new string[numberOfArticles];
+            public static double[] articlePrice = new double[numberOfArticles];
+            public static string[] articelName = new string[numberOfArticles];
+
+            #endregion arrays
+
+
+
+
+
+
+
+        }
+
+        class methods
+        {
+
+
+            public static bool NrOfArticles()
             {
 
-                Console.WriteLine("How many articles do you want? (From 1 to 10)");
-                numberOfArticles = Convert.ToInt32(Console.ReadLine());
+                bool result = false;
+                int nrOfArticles;
+                string Input = Console.ReadLine();
+                result = int.TryParse(Input, out nrOfArticles);
+
+
+                if (result && nrOfArticles != 0)
+                {
+                    Articles.numberOfArticles = nrOfArticles;
+                    result = true;
+
+                }
+                else return result; // returns false if tryparse failed
+
+                return result; // returns truee if tryparse succeded
 
             }
 
-           
-        
+            public static void ChangeConsole()
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Clear();
+
+                Console.Title = "Project A";
+                Console.WriteLine("Let's print a receipt");
+            }
+
+
+            public static string ChangeFontColor(string text)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                return text;
                 
-                
-            
-      
-            
-        
+            }
+
+
+
+
+
         }
+
+      
+
+
+
+
+
     }
 
 
@@ -96,27 +132,12 @@ namespace Project_A
 
 
            
-            int numberOfArticles = 0;
 
-            int[] articlePrices = new int [numberOfArticles];
-            string[] articelNames = new string[numberOfArticles];
+
+
+
             
 
-            Console.WriteLine("How many articles do you want? (From 1 to 10)");
-            numberOfArticles = Convert.ToInt32(Console.ReadLine());
-            
-            do
-            {
-               int i = 0; 
-               Console.WriteLine($"Write the name and the price for article #{i}" );
-               articles = Console.ReadLine();
-                
-                i++;
-
-                if(articles.Item1 == "" ){ }
-
-            } while (0 < numberOfArticles);
- 
  
  
  */
