bbaa6f6ad95e608c7719919403f47e723c6bb79a
Shohruh <shohruh.bozorov@edu.newton.se>
2021-11-10 16:22:16 +0100

    Fixade G delen

--- Project A/Program.cs
+++ Project A/Program.cs
@@ -2,142 +2,158 @@
 
 namespace Project_A
 {
+    struct Articles
+    {
+        public static int numberOfArticles;
+        public decimal price;
+        public string name;
+
+    }
+
     class Program
     {
+        
+        static Articles[] articles = new Articles[Articles.numberOfArticles];
+        const decimal vat = 0.25M;
+        static int k = 0;
 
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            methods.ChangeConsole();
-            string text1 = "How many articles do you want? (From 1 to 10)";
-            bool result = false;
-            string text2 = "\nWrong input, try again! Only numbers.";
-            
+
+            Console.Title = "Project A";
+            Console.WriteLine("Welcome to Project A \nLet's print a receipt");
             int i = 0;
+            bool result = false;
 
-            do
+            while (!result) //Loops untill the right value is entered
             {
-                Console.WriteLine(text1);
-                result = methods.NrOfArticles();
-                if (!result) 
+                Console.WriteLine("How many articles do you want? (From 1 to 10): ");
+                result = NrOfArticles();
+
+                if (!result)
                 {
-                    methods.ChangeFontColor(text2);
-                    Console.WriteLine(text2);
-                    Console.ForegroundColor = ConsoleColor.Yellow;
-                
-                }
-                    
+                    Console.Clear();
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Wrong input, try again! Only numbers.");
+                    Console.ForegroundColor = ConsoleColor.White;
 
-
-
-            } while (result == false);
-
-
-
-            Console.WriteLine(result);
-
-
-
-
-
-
-        }
-
-
-
-        class Articles
-        {
-            public static int numberOfArticles;
-
-            #region arrays
-
-            public static string[] mixedArticles = new string[numberOfArticles];
-            public static double[] articlePrice = new double[numberOfArticles];
-            public static string[] articelName = new string[numberOfArticles];
-
-            #endregion arrays
-
-
-
-
-
-
-
-        }
-
-        class methods
-        {
-
-
-            public static bool NrOfArticles()
-            {
-
-                bool result = false;
-                int nrOfArticles;
-                string Input = Console.ReadLine();
-                result = int.TryParse(Input, out nrOfArticles);
-
-
-                if (result && nrOfArticles != 0)
-                {
-                    Articles.numberOfArticles = nrOfArticles;
-                    result = true;
 
                 }
-                else return result; // returns false if tryparse failed
-
-                return result; // returns truee if tryparse succeded
 
             }
 
-            public static void ChangeConsole()
+
+            while (i < Articles.numberOfArticles)
             {
-                Console.ForegroundColor = ConsoleColor.Yellow;
-                Console.Clear();
 
-                Console.Title = "Project A";
-                Console.WriteLine("Let's print a receipt");
-            }
+                Console.WriteLine($"Enter name and price for article #{i}. In the format name; price (example Beer; 2,25): ");
+                result = ReadArticle();
 
+                if (result)
+                {
+                    i++;
+                }
 
-            public static string ChangeFontColor(string text)
-            {
-                Console.ForegroundColor = ConsoleColor.Red;
-                return text;
-                
-            }
+            } 
 
-
-
+            PrintReciept();
 
 
         }
 
-      
+
+    #region methods
+
+        public static bool NrOfArticles()
+        {
+            string input = Console.ReadLine();
+            bool result = int.TryParse(input, out int nrOfArticles);
+
+            if (result && nrOfArticles != 0 && nrOfArticles <= 10)
+            {
+                Articles.numberOfArticles = nrOfArticles;
+                return true; 
+            }
+
+            return false; 
+        }
+
+        
+        public static bool ReadArticle()
+        {
+            string[][] input = new string[Articles.numberOfArticles][];
+            string userInput = Console.ReadLine();
+            if (!userInput.Contains(';'))
+            {
+                Console.WriteLine("Use ;");
+                return false;
+            }
+
+            input[k] = userInput.Split(';');
 
 
+            string name = input[k][0];
+            decimal price = 0;
+                
+            bool stringIsNumber = int.TryParse(name, out int number0);
+            bool decimalIsANumber = decimal.TryParse(input[k][1], out price);
+
+            if (stringIsNumber)
+                Console.Clear(); Console.WriteLine("Name cannot be number");
+
+            if (string.IsNullOrWhiteSpace(name))
+                Console.Clear(); Console.WriteLine("Name cannot be whitespace");
+
+            if (string.IsNullOrEmpty(name))
+                Console.Clear(); Console.WriteLine("name cannot be empty");
+
+            if (!decimalIsANumber)
+                Console.Clear(); Console.WriteLine("price must be a number");
+
+
+            if (decimalIsANumber && price != 0 && !stringIsNumber && !string.IsNullOrEmpty(name))
+            {
+                articles[k].name = name;
+                articles[k].price = price;
+                k++;
+                return true;
+
+            }
+
+            return false; 
+
+        }
+
+        public static void PrintReciept()
+        {
+            decimal totalPrice = 0;
+            decimal tax;
+
+            Console.WriteLine($"Number of items purchased: {Articles.numberOfArticles}");
+
+            Console.WriteLine("{0,-20} {1,14} {2,18}", "#", "Name", "Price");
+
+            for (int i = 0; i < articles.Length; i++)
+            {
+                Console.WriteLine($"{i,-30} {articles[i].name} {articles[i].price,20:c}");
+                totalPrice += articles[i].price;
+            }
+
+            tax = totalPrice * vat;
+
+            Console.WriteLine();
+            Console.WriteLine("{0,-35} {1,20}", "Total purchase", $"{totalPrice:c}");
+            Console.WriteLine("{0,-35} {1,20}", "Includes VAT (25%):", $"{ tax:c}");
+
+        }
+        
+     #endregion methods
 
 
 
     }
 
 
-    
+
 }
 
-/*
- 
- 
- 
- 
-
-
-           
-
-
-
-
-            
-
- 
- 
- */
