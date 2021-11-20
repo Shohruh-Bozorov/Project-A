12be5157422058f5ae46734a33c325f85382d3df
Shohruh <shohruh.bozorov@edu.newton.se>
2021-11-20 19:07:33 +0100

    fixade ett fel

--- Project A/Program.cs
+++ Project A/Program.cs
@@ -2,19 +2,17 @@
 
 namespace Project_A
 {
-    struct Articles
-    {
-        public static int numberOfArticles;
-        public decimal price;
-        public string name;
-
-    }
-
     class Program
     {
-        
-        static Articles[] articles = new Articles[Articles.numberOfArticles];
-        const decimal vat = 0.25M;
+        struct Articles
+        {
+            public decimal price;
+            public string name;
+        }
+
+        static int NrOfArticles;
+        static Articles[] articles;
+        const decimal _vat = 0.25M;
         static int k = 0;
 
         public static void Main(string[] args)
@@ -25,10 +23,10 @@ namespace Project_A
             int i = 0;
             bool result = false;
 
-            while (!result) //Loops untill the right value is entered
+            while (!result)
             {
                 Console.WriteLine("How many articles do you want? (From 1 to 10): ");
-                result = NrOfArticles();
+                result = TryReadNrArticles(out NrOfArticles);
 
                 if (!result)
                 {
@@ -40,110 +38,102 @@ namespace Project_A
 
                 }
 
+                articles = new Articles[NrOfArticles];
+                
+
             }
 
-
-            while (i < Articles.numberOfArticles)
+            while (i < articles.Length)
             {
-
                 Console.WriteLine($"Enter name and price for article #{i}. In the format name; price (example Beer; 2,25): ");
-                result = ReadArticle();
+                result = TryReadArticle();
 
                 if (result)
                 {
                     i++;
                 }
-
             } 
 
             PrintReciept();
-
-
         }
 
 
     #region methods
 
-        public static bool NrOfArticles()
+        public static bool TryReadNrArticles(out int nrArticles)
         {
             string input = Console.ReadLine();
             bool result = int.TryParse(input, out int nrOfArticles);
 
             if (result && nrOfArticles != 0 && nrOfArticles <= 10)
             {
-                Articles.numberOfArticles = nrOfArticles;
+                nrArticles = nrOfArticles;
                 return true; 
             }
 
+            nrArticles = 0;
             return false; 
         }
 
         
-        public static bool ReadArticle()
+        public static bool TryReadArticle()
         {
-            string[][] input = new string[Articles.numberOfArticles][];
+            string[][] input = new string[NrOfArticles][];
             string userInput = Console.ReadLine();
             if (!userInput.Contains(';'))
             {
-                Console.WriteLine("Use ;");
+                Console.WriteLine("Format error");
                 return false;
             }
 
             input[k] = userInput.Split(';');
 
-
             string name = input[k][0];
             decimal price = 0;
-                
+ 
             bool stringIsNumber = int.TryParse(name, out int number0);
             bool decimalIsANumber = decimal.TryParse(input[k][1], out price);
 
-            if (stringIsNumber)
-                Console.Clear(); Console.WriteLine("Name cannot be number");
-
-            if (string.IsNullOrWhiteSpace(name))
-                Console.Clear(); Console.WriteLine("Name cannot be whitespace");
-
-            if (string.IsNullOrEmpty(name))
-                Console.Clear(); Console.WriteLine("name cannot be empty");
-
-            if (!decimalIsANumber)
-                Console.Clear(); Console.WriteLine("price must be a number");
-
-
-            if (decimalIsANumber && price != 0 && !stringIsNumber && !string.IsNullOrEmpty(name))
+            if (stringIsNumber || string.IsNullOrEmpty(name) || string.IsNullOrWhiteSpace(name))
             {
-                articles[k].name = name;
-                articles[k].price = price;
-                k++;
-                return true;
-
+                Console.WriteLine("Name error");
+                return false;
             }
 
-            return false; 
+
+            if (!decimalIsANumber || price == 0)
+            {
+                Console.WriteLine("Price error");
+                return false;
+            }
+
+            articles[k].name = name;
+            articles[k].price = price;
+            k++;
+            return true;
 
         }
 
         public static void PrintReciept()
         {
             decimal totalPrice = 0;
-            decimal tax;
+            decimal vat;
 
-            Console.WriteLine($"Number of items purchased: {Articles.numberOfArticles}");
+            Console.WriteLine($"Number of items purchased: {NrOfArticles}");
 
-            Console.WriteLine("{0,-20} {1,14} {2,18}", "#", "Name", "Price");
+            Console.WriteLine("{0} {1,15} {2,20}", "#", "Name", "Price");
 
             for (int i = 0; i < articles.Length; i++)
             {
-                Console.WriteLine($"{i,-30} {articles[i].name} {articles[i].price,20:c}");
+                Console.WriteLine($"{i} {articles[i].name, 15} {articles[i].price,20:c}");
                 totalPrice += articles[i].price;
             }
 
-            tax = totalPrice * vat;
+            vat = totalPrice * _vat;
 
             Console.WriteLine();
-            Console.WriteLine("{0,-35} {1,20}", "Total purchase", $"{totalPrice:c}");
-            Console.WriteLine("{0,-35} {1,20}", "Includes VAT (25%):", $"{ tax:c}");
+            Console.WriteLine("{0} {1,23}", "Total purchase", $"{totalPrice:c}");
+            Console.WriteLine("{0} {1,18}", "Includes VAT (25%):", $"{ vat:c}");
 
         }
         
